1d44b8c447b56e788b1007848b23370ea7119dc4
Alex-Rudoy <rudoy4ik@gmail.com>
2020-08-28 22:46:07 +0300

    initial commit

---
+++ app/js/index.js
@@ -0,0 +1,9 @@
+import "../scss/main.scss";
+
+class TestClass {
+  constructor() {
+    let msg = "Using ES2015+ syntax";
+    console.log(msg);
+  }
+}
+let test = new TestClass();
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // Require  html-webpack-plugin plugin
+
+module.exports = {
+  entry: __dirname + "/app/js/index.js", // webpack entry point. Module to start building dependency graph
+  output: {
+    path: __dirname + "/dist", // Folder to store generated bundle
+    filename: "bundle.js", // Name of generated bundle after build
+    publicPath: "/", // public URL of the output directory when referenced in a browser
+  },
+  module: {
+    // where we defined file patterns and their loaders
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: "babel-loader", // js polifill & transpiler
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          {
+            loader: "style-loader", // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader", // compiles Sass to CSS
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // Array of plugins to apply to build chunk
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.html",
+      inject: "body",
+    }),
+  ],
+  devServer: {
+    // configuration for webpack-dev-server
+    contentBase: "./app", //source of static assets
+    port: 3000, // port to run dev-server
+  },
+};
